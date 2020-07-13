8030749534061486ff55b2e81a2ede74598b5aef
whf <476459169@qq.com>
2020-07-13 17:13:36 +0800

    first up

---
+++ pages/login/regist.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="">
+		
+	</view>
+</template>
+
+<script>
+	
+	export default{
+			
+		data(){
+			return{
+				
+			}
+		},
+		
+		methods:{
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
